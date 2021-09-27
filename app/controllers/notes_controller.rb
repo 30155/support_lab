00b167d873afa2d573a643214ca14619e6030480
30155 <awlc.v41st@gmail.com>
2021-09-27 22:07:20 +0900

    notesコントローラーの追加(create)

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,4 +3,21 @@ class NotesController < ApplicationController
     @notes = Note.order(word_kana: "ASC")
     @note = Note.new
   end
+
+  def create
+    @notes = Note.order(word_kana: "ASC")
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to action: :index
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:word, :word_kana, :summary, :text, :reference).merge(user_id: current_user.id)
+  end
+  
 end
