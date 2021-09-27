6e6e664951cf58d5623ec4d295968c7835d6ee6b
30155 <awlc.v41st@gmail.com>
2021-09-27 22:28:16 +0900

    noteのルーティング設定(edit,update)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
-  resources :notes, only: [:index, :create,]
+  resources :notes, only: [:index, :create, :edit, :update]
 end
