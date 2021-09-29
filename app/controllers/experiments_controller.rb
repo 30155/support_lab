988bb00252d0ab9a7cd782a59045edbe41e64d36
30155 <awlc.v41st@gmail.com>
2021-09-29 18:39:02 +0900

    experimentsコントローラーの追加(create)

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -4,5 +4,17 @@ class ExperimentsController < ApplicationController
   end
 
   def create
+    @experiment = Experiment.new(experiment_params)
+    if @experiment.save
+      redirect_to action: :new
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def experiment_params
+    params.require(:experiment).permit(:datetime, :weather_id, :temperature, :humidity, :member, :title, :purpose, :method, :condition1, :condition2, :result, :consideration, :note).merge(user_id: current_user.id)
   end
 end
