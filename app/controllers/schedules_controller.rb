a40314a5a597d3bcd0b0db811e9d8805955baec1
30155 <awlc.v41st@gmail.com>
2021-10-01 15:04:44 +0900

    schedulesコントローラーの追加(index, create)

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,19 @@
 class SchedulesController < ApplicationController
+
+  def index
+    @schedules = Schedule.all
+    @schedule = Schedule.new
+  end
+
+  def create
+    Schedule.create(schedule_params)
+    redirect_to action: :index
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:datetime, :plan, :memo).merge(user_id: current_user.id)
+  end
+  
 end
