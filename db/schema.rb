220ca2848bcafe5fe051ebb25d9f3fb84e42a18e
30155 <awlc.v41st@gmail.com>
2021-10-07 17:57:17 +0900

    中間テーブルの作成(room_user)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_091350) do
+ActiveRecord::Schema.define(version: 2021_10_06_154902) do
 
   create_table "experiments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "datetime", null: false
@@ -44,6 +44,21 @@ ActiveRecord::Schema.define(version: 2021_10_05_091350) do
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "schedules", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "datetime", null: false
     t.string "plan", null: false
@@ -72,5 +87,7 @@ ActiveRecord::Schema.define(version: 2021_10_05_091350) do
 
   add_foreign_key "experiments", "users"
   add_foreign_key "notes", "users"
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
   add_foreign_key "schedules", "users"
 end
