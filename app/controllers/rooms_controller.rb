673c667a6fca5808baca178f6a35b7fd6ce5d324
30155 <awlc.v41st@gmail.com>
2021-10-07 18:17:12 +0900

    roomsコントローラーの追加(index, create)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,22 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to action: :index
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
