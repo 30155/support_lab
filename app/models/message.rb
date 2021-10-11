15820d6f58e94207fef83798736c659d1199112a
30155 <awlc.v41st@gmail.com>
2021-10-11 20:34:52 +0900

    アソシエーション、バリデーションの追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_many_attached :images
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.images.attached?
+  end
 end
