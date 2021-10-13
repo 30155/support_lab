32a23773ec7670208ea2d8ac819d76b42e692b07
30155 <awlc.v41st@gmail.com>
2021-10-13 18:13:32 +0900

    単体テスト(Messageモデル)

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :message do
+    content {Faker::Lorem.sentence}
+    association :user
+    association :room
+
+    after(:build) do |message|
+      message.images.attach(io: File.open('public/images/top-sample.jpg'), filename: 'top-sample.jpg')
+    end
   end
 end
